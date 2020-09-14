034e4820b01c7e941d9d8bfa49079c0714a5911d
guillempuigcom <guillem.puig@goldenspear.com>
2020-09-14 17:31:13 +0200

    update entrypoint to handle mandatory and non mandatory params

--- entrypoint.py
+++ entrypoint.py
@@ -8,11 +8,21 @@ import jenkins
 import time
 import json
 
-JENKINS_URL = sys.argv[1]
-JENKINS_TOKEN = sys.argv[2]
-JENKINS_USER = sys.argv[3]
-JOB_PATH = sys.argv[4]
-JOB_PARAMS = sys.argv[5]
+
+def mandatory_arg(argv):
+    if argv == "":
+        raise ValueError("Only job_params can be empty. Required fields: url, token, user and path")
+    return argv
+
+
+# mandatory
+JENKINS_URL = mandatory_arg(sys.argv[1])
+JENKINS_TOKEN = mandatory_arg(sys.argv[2])
+JENKINS_USER = mandatory_arg(sys.argv[3])
+JOB_PATH = mandatory_arg(sys.argv[4])
+
+# not mandatory
+JOB_PARAMS = sys.argv[5] or '{}'
 
 # create/connect jenkins server
 server = jenkins.Jenkins(f"http://{JENKINS_URL}", username=JENKINS_USER, password=JENKINS_TOKEN)
