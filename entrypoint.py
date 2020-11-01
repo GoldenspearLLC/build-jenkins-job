12b6049dd81d081d6d506241afa2799ec14e6268
Shaked Shauli <shaked@curv.co>
2020-11-01 13:58:01 +0200

    DevOps:  more changes

--- entrypoint.py
+++ entrypoint.py
@@ -20,15 +20,13 @@ JENKINS_URL = mandatory_arg(sys.argv[1])
 JENKINS_TOKEN = mandatory_arg(sys.argv[2])
 JENKINS_USER = mandatory_arg(sys.argv[3])
 JOB_PATH = mandatory_arg(sys.argv[4])
-
 # not mandatory
 JOB_PARAMS = sys.argv[5] or '{}'
 
 # create/connect jenkins server
 server = jenkins.Jenkins(f"https://{JENKINS_URL}", username=JENKINS_USER, password=JENKINS_TOKEN)
 user = server.get_whoami()
-version = server.get_version()
-print(f"Hello {user['fullName']} from Jenkins {version}")
+print(f"Triggering Jenkins with user {user['fullName']}")
 
 # build job
 split = JOB_PATH.split("job/")
